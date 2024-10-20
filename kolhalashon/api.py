1cae90d43d0aa803412ed530031b8a3ea70e5cd1
zviShteinman <t7647986@gmail.com>
2024-10-20 05:01:30 +0300

    Fixed bug not downloading files

--- kolhalashon/api.py
+++ kolhalashon/api.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
-from typing import List, Dict
-from .models.shiur import Shiur, ShiurDetails, Category, QualityLevel
+from typing import List, Dict, Generator
+
+from sympy import Li
+from .models.shiur import Shiur, ShiurDetails, QualityLevel
 from .models.exceptions import *
 from .utils.session_manager import SessionManager
 
@@ -11,16 +13,10 @@ class KolHalashonAPI:
     def __init__(self, username: str = None, password: str = None, use_session=False, session_file='session.pkl'):
         self.username = username if username else os.getenv('KOL_HALASHON_USERNAME', '')
         self.password = password if password else os.getenv('KOL_HALASHON_PASSWORD', '')
-        self.base_url = "https://www2.kolhalashon.com:444/api/"
-        self.headers = {
-            'accept': 'application/json, text/plain, */*',
-            'accept-language': 'he-IL,he;q=0.9,en-AU;q=0.8,en;q=0.7,en-US;q=0.6',
-            'authorization-site-key': 'Bearer 8ea2pe8',
-            'cache-control': 'no-cache',
-            'content-type': 'application/json',
+        self._base_url = "https://www2.kolhalashon.com:444/api/"
+        self._headers = {
             'origin': 'https://www2.kolhalashon.com',
             'referer': 'https://www2.kolhalashon.com/',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
         }
         
         self.use_session = use_session
@@ -38,34 +34,36 @@ class KolHalashonAPI:
             print("Session not found or invalid, attempting to login.")
             self.__login(self.username, self.password)
 
-
     def __login(self, username: str, password: str):
-        if not self.use_session:
-            print("Session disabled, login not required.")
-            return False
-        
-        login_url = f"{self.base_url}Accounts/UserLogin/"
+        if not username or not password:
+            raise AuthenticationError("Username and password are required")
+
+        login_url = f"{self._base_url}Accounts/UserLogin/"
         payload = {"Username": username, "Password": password}
-        response = self.session_manager.session.post(login_url, json=payload, headers=self.headers)
+        response = self.session_manager.session.post(login_url, json=payload, headers=self._headers)
         
         if response.status_code == 200:
             data = response.json()
             token = data.get('Token')
             if token:
-                self.session_manager.set_token(token)
+                self.session_manager.set_tokens(token, data.get('SiteKey', ''))
                 return True
-            raise AuthenticationError("Login successful but no token found.")
+            raise AuthenticationError("Login successful but no token found")
         raise AuthenticationError(f"Login failed with status code {response.status_code}")
 
-    def search_items(self, keyword: str, user_id: int = -1) -> Category:
-        url = f"{self.base_url}Search/WebSite_GetSearchItems/{keyword}/{user_id}/1/4"
-        response = self.session_manager.session.get(url, headers=self.headers)
-        if response.status_code == 200:
-            return self.categorize_items(response.json())
-        raise SearchFailedException(f"Error fetching data for keyword: {keyword}", response.status_code)
 
-    def search_rav_shiurim(self, rav_id: int) -> List[Shiur]:
-        url = f"{self.base_url}Search/WebSite_GetRavShiurim/"
+    def search_items(self, keyword: str, user_id: int = -1) -> List[Dict]:
+        url = f"{self._base_url}Search/WebSite_GetSearchItems/{keyword}/{user_id}/1/4"
+        response = self.session_manager.session.get(url, headers=self._headers)
+        if response.status_code == 200:
+            return response.json()  # Return the items directly
+        else:
+            print(f"Request failed with status code {response.status_code} and response: {response.text}")
+            raise SearchFailedException(f"Error fetching data for keyword: {keyword}", response.status_code)
+
+
+    def search_rav_shiurim(self, rav_id: int) -> Generator[Shiur, None, None]:
+        url = f"{self._base_url}Search/WebSite_GetRavShiurim/"
         data = {
             "QueryType": -1,
             "LangID": -1,
@@ -76,23 +74,25 @@ class KolHalashonAPI:
             "GeneralID": rav_id,
             "FilterSwitch": "1" * 90
         }
-        response = self.session_manager.session.post(url, headers=self.headers, json=data)
+        response = self.session_manager.session.post(url, headers=self.session_manager.headers, json=data)
         if response.status_code == 200:
-            return self.format_shiurim(response.json())
-        raise SearchFailedException(f"Error fetching Rav Shiurim for Rav ID: {rav_id}", response.status_code)
+            yield from self.format_shiurim(response.json())
+        else:
+            raise SearchFailedException(f"Error fetching Rav Shiurim for Rav ID: {rav_id}", response.status_code)
+
 
     def download_file(self, file_id: int, quality_level: QualityLevel) -> str:
         if not self.use_session:
             raise SessionDisabledException()
 
         download_key = self.session_manager.get_download_key(file_id)
-        url = f"{self.base_url}files/GetFileDownload/{file_id}/{quality_level.value}/{download_key}/null/false/false"
-        
+        url = f"{self._base_url}files/GetFileDownload/{file_id}/{quality_level.value}/{download_key}/null/false/false"
+        print(url)
         file_extension = 'mp3' if quality_level == QualityLevel.AUDIO else 'mp4'
         quality_name = 'audio' if quality_level == QualityLevel.AUDIO else 'video' if quality_level == QualityLevel.VIDEO else 'hd'
         file_name = f"shiur_{file_id}_{quality_name}.{file_extension}"
 
-        response = self.session_manager.session.get(url, headers=self.headers, stream=True)
+        response = self.session_manager.session.get(url, headers=self.session_manager.headers, stream=True)
         if response.status_code == 200:
             with open(file_name, 'wb') as file:
                 for chunk in response.iter_content(chunk_size=8192):
@@ -100,33 +100,22 @@ class KolHalashonAPI:
             return file_name
         raise DownloadFailedException("Download failed", response.status_code, file_id, quality_level)
 
+
+
     def get_shiur_details(self, file_id: int) -> ShiurDetails:
-        url = f"{self.base_url}TblShiurimLists/WebSite_GetShiurDetails/{file_id}"
-        response = self.session_manager.session.get(url, headers=self.headers)
+        url = f"{self._base_url}TblShiurimLists/WebSite_GetShiurDetails/{file_id}"
+        response = self.session_manager.session.get(url, headers=self.session_manager.headers)
         
         if response.status_code == 200:
             return self._parse_shiur_details(response.json())
         raise ShiurDetailsNotFoundException(file_id)
+    
+
 
     @staticmethod
-    def categorize_items(items: List[Dict]) -> Category:
-        categories = Category(rabanim=[], books=[], shiurim=[], others=[])
-        for item in items:
-            search_item_type = item.get("SearchItemType")
-            if search_item_type == 2:
-                categories.rabanim.append(item)
-            elif search_item_type == 8:
-                categories.books.append(item)
-            elif search_item_type == 10:
-                categories.shiurim.append(item)
-            else:
-                categories.others.append(item)
-        return categories
-
-    @staticmethod
-    def format_shiurim(shiurim: List[Dict]) -> List[Shiur]:
-        return [
-            Shiur(
+    def format_shiurim(shiurim: List[Dict]) -> Generator[Shiur, None, None]:
+        for shiur in shiurim:
+            yield Shiur(
                 file_id=shiur.get("FileId", 0),
                 title=shiur.get("TitleHebrew", ""),
                 rav=shiur.get("UserNameHebrew", ""),
@@ -142,9 +131,8 @@ class KolHalashonAPI:
                 women_only=shiur.get("IsWomenOnly", False),
                 shiur_type=shiur.get("ShiurType", "Unavailable"),
                 viewed_by_user=shiur.get("ViewdByUser", False)
-            ) for shiur in shiurim
-        ]
-
+            )
+            
     @staticmethod
     def _parse_shiur_details(data: Dict) -> ShiurDetails:
         return ShiurDetails(
